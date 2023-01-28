678c9add64eee388dc021afc98a3f30538c1b7bc
Manish kumar <91480902+manni2000@users.noreply.github.com>
2023-01-28 12:23:21 +0530

    Update

---
+++ Binary_Tree/Data Stream as Disjoint Intervals.cpp
@@ -0,0 +1,42 @@
+class SummaryRanges {
+    set<int>values;
+
+public:
+    SummaryRanges() {}
+    
+    void addNum(int value) {
+        values.insert(value);
+    }
+    
+    vector<vector<int>> getIntervals() {
+        if(values.empty()){
+            return {};
+        }
+
+        vector<vector<int>>ans;
+        int left =-1;
+        int right=-1;
+
+        for(int value: values){
+           if(left<0){
+               left=right=value;
+           }
+           else if(value == right+1){
+               right=value;
+           }
+           else{
+               ans.push_back({left,right});
+               left=right=value;
+           }
+        }
+        ans.push_back({left,right});
+        return ans;
+    }
+};
+
+/**
+ * Your SummaryRanges object will be instantiated and called as such:
+ * SummaryRanges* obj = new SummaryRanges();
+ * obj->addNum(value);
+ * vector<vector<int>> param_2 = obj->getIntervals();
+ */
